48dec37f9d25694089812e959fab0edb10b37b13
minjae <alswo2722@naver.com>
2019-08-09 21:07:27 +0900

    Project init

---
+++ jjanhaeyo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jjanhaeyo
+//
+//  Created by minjae on 09/08/2019.
+//  Copyright © 2019 minjae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
