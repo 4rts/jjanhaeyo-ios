4bebd7408e6186e986bfc6d2eff8171cbf1f4ea5
minjae <alswo2722@naver.com>
2019-09-08 18:14:56 +0900

    라인 스페이싱 확장

--- jjanhaeyo/Utils/TextExtension.swift
+++ jjanhaeyo/Utils/TextExtension.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2019 minjae. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UILabel {
+    func setLineSpacing(_ text: String, lineSpacing: CGFloat) {
+        let attributedString = NSMutableAttributedString(string: text)
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = lineSpacing
+        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value:paragraphStyle, range:NSMakeRange(0, attributedString.length))
+        
+        self.attributedText = attributedString
+    }
+}
